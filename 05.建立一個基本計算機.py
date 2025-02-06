a56f3986ddaf150286ec38d83d4d72502dcfac9f
Audin <odinw86@gmail.com>
2025-02-06 22:07:39 +0800

    lesson 5

---
+++ 05.建立一個基本計算機.py
@@ -0,0 +1,13 @@
+
+name = input("請輸入名字: ")
+
+number1 = input("請輸入第一個加數: ");
+number2 = input("請輸入第二個加數: ");
+string_result = number1 + number2
+
+print("字串相加(預設)")
+print(string_result)
+
+int_result = int(number1) + int(number2)
+print("整數相加")
+print(int_result)
