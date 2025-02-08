feef6a0fcd7d36877dcd339b696f5463bbf16ea8
Audin <odinw86@gmail.com>
2025-02-08 20:45:34 +0800

    lesson 7

---
+++ 07.元組tuple.py
@@ -0,0 +1,15 @@
+
+# https://youtu.be/gM-puiT5lu8?si=pNtPAHjurRn63JQn
+
+scores = (5,1,9,2,3)
+
+print("tuple 大部分方法都跟 list 很像")
+print("差在 tuple 的值不能被更改，用來防止被誤改")
+print("差在 tuple 的值不能被更改，用來防止被誤改")
+#scores[0] = 0 #會報錯
+
+print("取得指定索引")
+print(scores[2])
+
+print("tuple長度")
+print(len(scores))
