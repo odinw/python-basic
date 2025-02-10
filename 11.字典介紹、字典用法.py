502ac0131bd0edbc487ca9fce42ca50d1920e2b6
Audin <odinw86@gmail.com>
2025-02-10 21:49:59 +0800

    lesson 11 dictionary

---
+++ 11.字典介紹、字典用法.py
@@ -0,0 +1,13 @@
+# https://youtu.be/DOWswV0CASg?si=WehoURc4J_acrwqZ
+
+fruits = {"蘋果":"apple", "香蕉":"banana"}
+print("字串key")
+print(fruits)
+print(fruits["蘋果"])
+#print(fruits["貓"]) #不存在的key會報錯
+
+player = {1:"Audin", 2:"Tom"}
+print("數值key")
+print(player)
+print(player[1])
+#print(player[0]) #不存在的key會報錯
