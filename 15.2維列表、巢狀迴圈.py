1717384ccfa545aecc0bdebd42762c70e2105a62
Audin <odinw86@gmail.com>
2025-02-14 21:28:12 +0800

    lesson 15.

---
+++ 15.2維列表、巢狀迴圈.py
@@ -0,0 +1,15 @@
+# https://youtu.be/pEPlcN7gOVs?si=wD6iTzJgu_6KBkZJ
+
+nums = [
+    [1, 2], 
+    [3,4],
+    [5]
+]
+
+print("取指定成員:")
+print(nums[1][0])
+
+print("遍巡:")
+for row in nums:
+    for col in row:
+        print(col)
