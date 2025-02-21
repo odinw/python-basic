ab58ac8a625b2a5772f53c6d1fa4e8db04e31556
Audin <odinw86@gmail.com>
2025-02-21 22:49:16 +0800

    lesson 18. class object

---
+++ 18.類別class、物件object 介紹.py
@@ -0,0 +1,12 @@
+# https://youtu.be/_AgguyVzt4o?si=3ftvlcIlD_7tUCKj
+
+class Phone:
+    def __init__(self, os, number):
+        self.os = os
+        self.number = number
+
+phone1 = Phone("ios", 1)
+print(phone1.os)
+
+phone2 = Phone("android", 2)
+print(phone2.number)
