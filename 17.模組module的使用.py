5cc8b0f71d763145b5436f6cba8584b935dd2fa6
Audin <odinw86@gmail.com>
2025-02-23 11:06:43 +0800

    refact: lesson17's folder name

--- 17.模組module的使用.py
+++ 17.模組module的使用.py
@@ -1,7 +1,7 @@
 #https://youtu.be/SV5OqR46kEM?si=FdFhm8ioGqNPX7ku
 
 # import 相對目錄下的自寫腳本，並改名(簡化名稱)來用
-from function_lesson_17 import import_call_function as call
+from lesson_17 import import_call_function as call
 
 
 print("呼叫自寫的模組:")
